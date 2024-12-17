b35efb0b12aa08f191959cf865424b9a3874d7eb
Jon Stone <54716195+jondstone@users.noreply.github.com>
2024-12-17 13:28:18 +0200

    add form load time vs submission time check
    adding a hidden field to the contact form did not seem to stop the spam emails.
    
    So, I decided to add a submission time check. If the submit button is clicked less than 5 seconds after the page is loaded, it will not submit the form.
    
    It was my belief these bots auto populate text, and click submit instantly.
    
    Tested and validated, and it's been 5 days since the prod change, and not a single spam email (usually 2 per day)

--- ContactForm/index.php
+++ ContactForm/index.php
@@ -21,58 +21,56 @@
  		<!-- Contact Page content removed as it's not necessary to be displayed in Git.-->
 
  		<div id="form">
- 			<form method="post" action="index.php" name="contactform" onsubmit="return validate.check(this)">
+			<form method="post" action="index.php" name="contactform" onsubmit="return validate.check(this)">
+				<input type="hidden" name="formLoadTime" value="<?php echo time() * 1000; ?>">
+				<input type="text" name="extra_name" id="extra_name" style="display:none;">
+
 				<table class="contactform">
 					<tr>
-					 <td colspan="2" style="padding-bottom:10px">
-					 	<div class="contactformmessage">Fields marked with a <span class="required_star">*</span> are required.</div>
-					 </td>
+						<td colspan="2" style="padding-bottom:10px">
+					 		<div class="contactformmessage">Fields marked with a <span class="required_star">*</span> are required.</div>
+					 	</td>
 					</tr>
 					<tr>
-					 <td valign="top">
-					  <label for="Full_Name" class="required">Full Name: <span class="required_star"> * </span></label>
-					 </td>
-					 <td valign="top">
-					  <input type="text" name="Full_Name" id="Full_Name" maxlength="50" style="width:230px">
-					 </td>
+					 	<td valign="top">
+					  		<label for="Full_Name" class="required">Full Name: <span class="required_star"> * </span></label>
+					 	</td>
+					 	<td valign="top">
+					  		<input type="text" name="Full_Name" id="Full_Name" maxlength="50" style="width:230px">
+					 	</td>
 					</tr>
 					<tr>
-					 <td valign="top">
-					  <label for="Email_Address" class="required">Email Address: <span class="required_star"> * </span></label>
-					 </td>
-					 <td valign="top">
-					  <input type="text" name="Email_Address" id="Email_Address" maxlength="50" style="width:230px">
-					 </td>
+					 	<td valign="top">
+					  		<label for="Email_Address" class="required">Email Address: <span class="required_star"> * </span></label>
+					 	</td>
+					 	<td valign="top">
+					  		<input type="text" name="Email_Address" id="Email_Address" maxlength="50" style="width:230px">
+					 	</td>
 					</tr>
 					<tr>
-					 <td valign="top">
-					  <label for="Telephone_Number" class="not-required">Telephone:</label>
-					 </td>
-					 <td valign="top">
-					  <input type="text" name="Telephone_Number" id="Telephone_Number" maxlength="20" style="width:230px">
-					 </td>
+					 	<td valign="top">
+					  		<label for="Telephone_Number" class="not-required">Telephone:</label>
+					 	</td>
+						 <td valign="top">
+					  		<input type="text" name="Telephone_Number" id="Telephone_Number" maxlength="20" style="width:230px">
+					 	</td>
 					</tr>
 					<tr>
-					 <td valign="top">
-					  <label for="Comments" class="required">Comments: <span class="required_star"> * </span></label>
-					 </td>
-					 <td valign="top">
-					  <textarea style="width:230px;height:160px" name="Your_Message" id="Your_Message" maxlength="2000"></textarea>
-					 </td>
+					 	<td valign="top">
+					  		<label for="Comments" class="required">Comments: <span class="required_star"> * </span></label>
+					 	</td>
+					 	<td valign="top">
+					  		<textarea style="width:230px;height:160px" name="Your_Message" id="Your_Message" maxlength="2000"></textarea>
+					 	</td>
 					</tr>
 					<tr>
-					 <td>
-					  <label for="AntiSpam" class="required">5 plus 7 is? <span class="required_star"> * </span>&nbsp;</label>
-					 </td>
-					 <td valign="top">
-					  <input type="text" name="AntiSpam" id="AntiSpam" maxlength="2" style="width:25px">
-					 </td>
+					 	<td>
+					  		<label for="AntiSpam" class="required">5 plus 7 is? <span class="required_star"> * </span>&nbsp;</label>
+					 	</td>
+					 	<td valign="top">
+					  		<input type="text" name="AntiSpam" id="AntiSpam" maxlength="2" style="width:25px">
+					 	</td>
 					</tr>
-					<tr>
-						<td>
-							<input type="text" name="extra_name" id="extra_name" style="display:none;">
-						</td>
-					</tr> 
 					<tr>
 						<td valign="top">
 							<input type="submit" name="submit" value="Submit Inquiry" style="width:125px;height:25px">
@@ -88,6 +86,7 @@
 				$telephone_number = '';
 				$your_message = '';
 				$anti_spam = '';
+				$form_load_time = 0;
 
 				// Only proceed if the form is submitted via POST
 				if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
@@ -97,19 +96,29 @@
 					$telephone_number = isset($_POST['Telephone_Number']) ? trim($_POST['Telephone_Number']) : '';
 					$your_message = isset($_POST['Your_Message']) ? trim($_POST['Your_Message']) : '';
 					$anti_spam = isset($_POST['AntiSpam']) ? $_POST['AntiSpam'] : '';
+					$form_load_time = isset($_POST['formLoadTime']) ? $_POST['formLoadTime'] : 0;
 
-					// Anti-spam check: if the AntiSpam field contains anything, block the form submission
+					// Time-based check: Reject form if submitted too quickly (less than 5 seconds)
+					$form_submit_time = time() * 1000; // Current timestamp in milliseconds
+					$time_spent = ($form_submit_time - $form_load_time) / 1000; // Time spent in seconds
+
+					// If time spent is too fast, kill the submission
+					if ($time_spent < 5) {
+						die('Form submission detected as bot activity. Please take your time.');
+					}
+
+					// Anti-spam check: if the extra_name field contains anything, kill the submission
 					if (!empty($extra_name)) {
-						die('Submission blocked. You know why! :)');
+						die('Submission reject! You know why :)');
 					}
 
 					// Submit Form
 					if (!empty($full_name) && !empty($email_address) && !empty($your_message) && $anti_spam === '12') {
 						// Prepare email headers and body
-						$headers = 'From: ' . $email_address . "\r\n" .
-								'Reply-To: ' . $email_address . "\r\n" .
+						$headers = 'From: ' . $email_address . "\r\n" . 
+								'Reply-To: ' . $email_address . "\r\n" . 
 								'X-Mailer: PHP/' . phpversion();
-						$to = 'info@jondstone.com';
+						$to = 'name@email.com';
 						$subject = 'Inquiry';
 						$body = "From: $full_name\nE-Mail: $email_address\nPhone: $telephone_number\nComments:\n$your_message";
 
@@ -117,7 +126,7 @@
 						if (mail($to, $subject, $body, $headers)) {
 							echo '<p>Thank you, your inquiry has been received!</p>';
 						} else {
-							echo '<p>Something went wrong, please try again!</p>';
+							echo '<p>Something went wrong, please try again or send me an email directly!</p>';
 						}
 					} else {
 						// If the anti-spam field is empty or incorrect, show a message to the user
